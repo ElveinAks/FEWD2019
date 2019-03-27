c0e1d6e203151ac73359733c73dd1dca3c94211e
Elvein <e0aksa01@louisville.edu>
2019-03-26 20:04:21 -0400

    JS with comments

--- howilearnedlanguages.js
+++ howilearnedlanguages.js
@@ -1,3 +1,4 @@
-$('.nav-item').hide().delay(1000).slideDown(2000);
-$('#page-title'),
+$('.nav-item').hide().delay(1000).slideDown(2000); //Hides the nav-links and then has them slide down into view.
+
+$('#page-title'),                                  //Hide header title and content, has them fade into view.
 $('.container-fluid').hide().fadeIn(4000);
